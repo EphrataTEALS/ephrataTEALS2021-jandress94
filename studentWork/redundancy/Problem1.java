19a817006cc1428a850859896612fa56522b2eff
Jim Andress <jandress@lirio.co>
2021-10-18 20:45:58 -0700

    adding new starter files

---
+++ studentWork/redundancy/Problem1.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int x = console.nextInt();
+
+        int a = 0;
+
+        if (x < 30) {
+            a = 2;
+            x++;
+            System.out.println("Spongebob Squarepants! " + x);
+        } else {
+            a = 2;
+            System.out.println("Spongebob Squarepants! " + x);
+        }
+    }
+}
