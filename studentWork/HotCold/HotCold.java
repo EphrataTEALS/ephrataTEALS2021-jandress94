7180a3db700ffd1401e36f10237cf357c775c4d9
jandress94 <jim.andress@gmail.com>
2021-11-17 16:52:00 +0000

    add comments for HotCold

--- studentWork/HotCold/HotCold.java
+++ studentWork/HotCold/HotCold.java
@@ -16,19 +16,26 @@ public class HotCold {
         System.out.print("Enter hotter, colder, or correct: ");
         String input = console.next();
 
+        // The count starts off as 0
         int count = 0;
 
+        // we want to continue looping until they finally enter "correct", at which point we stop
         while (!input.equals("correct")) {
             if (input.equals("hotter") || input.equals("colder")) {
+                // we only want to increment the count if they entered valid input
                 System.out.println("you entered " + input);
                 count++;
             } else {
+                // if they entered bad input, let them know
                 System.out.println("That is not a valid input");
             }
 
+            // ask for the next input
             System.out.print("Enter hotter, colder, or correct: ");
             input = console.next();
         }
+
+        // we have to add one to make sure that we account for the input of "correct"
         count++;
 
         System.out.println("You entered correct, and it took " + count + " guesses.");
